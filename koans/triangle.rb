c156a4cd2f3ea3d60ec23dff243e369d60300248
Franklin Huichi <carloshuichi@gmail.com>
2022-03-05 18:20:40 -0500

    Testing three koans and one class
    I have tested about_triangle_project, , about_exceptions  ,about_triangle_project_2 and the class Triangle

--- koans/triangle.rb
+++ koans/triangle.rb
@@ -13,10 +13,34 @@
 # and
 #   about_triangle_project_2.rb
 #
-def triangle(a, b, c)
-  # WRITE THIS CODE
+def triangle(side_a, side_b, side_c)
+  sides_array = [side_a, side_b, side_c]
+  raise_errors(sides_array)
+  case triangle_counts(sides_array)
+  when 1
+    :scalene
+  when 2
+    :isosceles
+  when 3
+    :equilateral
+  end
+end
+
+def raise_errors(sides_array)
+  raise TriangleError if sides_array.min <= 0 
+
+  x, y, z = sides_array.sort 
+  raise TriangleError if x + y <= z
+end
+def triangle_counts(sides_array, answer = 0)
+  sides_array.each do |side|
+    count_side = sides_array.count(side)
+    answer = count_side if count_side > answer
+  end
+  answer
 end
 
 # Error class used in part 2.  No need to change this code.
 class TriangleError < StandardError
+
 end
